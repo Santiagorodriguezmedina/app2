c3ea838aad5573a72a8c2b67c19f3f4e666d9823
Santiagorodriguezmedina <164428114+Santiagorodriguezmedina@users.noreply.github.com>
2024-04-10 00:36:11 +0000

    hey

--- Hello.py
+++ Hello.py
@@ -1,51 +1,23 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
+import pandas as pd
 
-LOGGER = get_logger(__name__)
+dataset = pd.read_csv("data.csv")
+
+x = dataset.iloc[:,0]
+y = dataset.iloc[:,1]
+image_url = "https://ibb.co/1sJPZwb"
 
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
-
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+# Streamlit app
+def main():
+    st.sidebar.title("VRS QBR")
+    selected_slide = st.sidebar.selectbox("Select Slide:", list(zip(x, y)), format_func=lambda item: item[0])
+    if selected_slide:
+        slide_name, link = selected_slide
+        st.markdown(f"[{slide_name}]({link})")
+        st.image(image_url, use_column_width=True)
 
 
 if __name__ == "__main__":
-    run()
+    main()
+    
